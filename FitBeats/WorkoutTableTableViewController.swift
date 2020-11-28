391cad2df295e84a61cc6a876965526228cb0870
dillonshipley <dshipley@zagmail.gonzaga.edu>
2020-11-28 13:16:44 -0800

    Adrian's changes

--- FitBeats/WorkoutTableTableViewController.swift
+++ FitBeats/WorkoutTableTableViewController.swift
@@ -4,52 +4,52 @@
 //
 //  Created by Dillon Shipley on 11/28/20.
 //
-
 import UIKit
+import CoreData
 
 class WorkoutTableTableViewController: UITableViewController {
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var workouts = [Workout]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if section == 0 {
+            return workouts.count
+        }
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         // Configure the cell...
+        let workout = workouts[indexPath.row]
+        cell.textLabel?.text = workout.name
 
         return cell
     }
-    */
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -57,14 +57,12 @@ class WorkoutTableTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
     }
     */
 
@@ -75,10 +73,30 @@ class WorkoutTableTableViewController: UITableViewController {
         return true
     }
     */
+    
+    func saveWorkouts() {
+        do {
+            try context.save()
+        }
+        catch {
+            print("Error saving workouts \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadWorkouts() {
+        let request: NSFetchRequest<Workout> = Workout.fetchRequest()
+        do {
+            workouts = try context.fetch(request)
+        }
+        catch {
+            print("Error loading workouts \(error)")
+        }
+        tableView.reloadData()
+    }
 
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
