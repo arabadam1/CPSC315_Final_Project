0fca5935a6bacbc23c030fb20c3bac1494e0268a
Adrian Rabadam <arabadam@zagmail.gonzaga.edu>
2020-12-13 23:24:14 -0800

    Pause, skip, workout ended implemented in Start VC

--- FitBeats/StartTableViewController.swift
+++ FitBeats/StartTableViewController.swift
@@ -52,6 +52,11 @@ class StartTableViewController: UITableViewController {
         })
     }
     
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             if section == 0 {
                 return exercises.count
@@ -73,15 +78,18 @@ class StartTableViewController: UITableViewController {
         return cell
     }
     
-    @IBAction func pauseExercise(_ sender: UIButton) {
+    @IBAction func pauseExercise(_ sender: UIBarButtonItem) {
         let indexPath = IndexPath(row: 0, section: 0)
         let cell = tableView.cellForRow(at: indexPath) as? ActiveExerciseCell
-        if cell?.timer != nil {
+        cell?.stopTimer()
+        self.stopTimer()
+        let alertController = UIAlertController(title: "Paused", message: "Your workout is currently paused", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Resume", style: .default, handler: {_ in
             cell?.startTimer()
-        }
-        else {
-            cell?.stopTimer()
-        }
+            self.startTimer()
+            alertController.dismiss(animated: true, completion: nil)
+        }))
+        present(alertController, animated: true)
     }
     
     @IBAction func skipExercise(_ sender: UIButton) {
@@ -93,24 +101,43 @@ class StartTableViewController: UITableViewController {
         exercises.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
         timer?.invalidate()
+        
+        var transitionAlert : UIAlertController = UIAlertController()
+        
         var transitionTimer : Timer? = nil
         var transitionSecondsLeft: Int = 15 {
             didSet {
                 if (transitionSecondsLeft == 0) {
                     transitionTimer?.invalidate()
                     transitionTimer = nil
-                    alertController.dismiss(animated: true, completion: nil)
+                    transitionAlert.dismiss(animated: true, completion: nil)
                     tableView.reloadData()
                 }
-                alertController.message = "Next exercise in: \(transitionSecondsLeft)"
+                transitionAlert.message = "Next exercise in: \(transitionSecondsLeft)"
             }
         }
-        let alertController = UIAlertController(title: "Transition", message: "Change exercise!", preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
-        present(alertController, animated: true) {
-            transitionTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
-                transitionSecondsLeft -= 1
-            })
+        if exercises.count == 0 {
+            let alertController = UIAlertController(title: "Workout Completed", message: "You have finished your workout!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Okay", style: .default, handler: {_ in
+                self.stopTimer()
+                alertController.dismiss(animated: true, completion: nil)
+            }))
+            present(alertController, animated: true)
+        }
+        else {
+            transitionAlert = UIAlertController(title: "Transition", message: "Change exercise!", preferredStyle: .alert)
+            transitionAlert.addAction(UIAlertAction(title: "Skip", style: .default, handler: {_ in
+                transitionTimer?.invalidate()
+                transitionTimer = nil
+                transitionAlert.dismiss(animated: true, completion: nil)
+                self.tableView.reloadData()
+            }))
+            
+            present(transitionAlert, animated: true) {
+                transitionTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
+                    transitionSecondsLeft -= 1
+                })
+            }
         }
     }
     
@@ -128,6 +155,11 @@ class StartTableViewController: UITableViewController {
             print("Error loading items \(error)")
         }
         tableView.reloadData()
+    }
+    
+    func changeBarButtonTitle(item: UIBarButtonItem, title: String) {
+        let button = item.customView as? UIButton
+        button?.setTitle(title, for: .normal)
     }
 
 }
